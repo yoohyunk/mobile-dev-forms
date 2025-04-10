ecf1f57da79e52244acac5bb8c83c782fded93a4
yoohyunk <yoohyunk20@gmail.com>
2025-04-10 02:37:01 -0600

    add employeelist page to tab bar

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import EmployeeFormScreen from "../screens/EmployeeFormScreen";
 import SignInScreen from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
+import EmployeeListScreen from "../screens/EmployeeListScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -25,6 +26,11 @@ export default function TabNavigator() {
           name="SignUp"
           component={SignUpScreen}
           options={{ title: "Sign Up" }}
+        />
+        <Tab.Screen
+          name="EmployeeList"
+          component={EmployeeListScreen}
+          options={{ title: "Employee List" }}
         />
       </Tab.Navigator>
     </NavigationContainer>
